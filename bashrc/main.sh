cebe1fb071431ffcf8cdcadc5f122d86a3a0bd8f
vpiskunov <xxxbobrxxx@gmail.com>
2018-09-21 18:00:07 +0300

    cleanup

--- bashrc/main.sh
+++ bashrc/main.sh
@@ -1,9 +1,9 @@
-
-
-
+#: Global definitions
 export VA_SYSTEM_CONFIG_ROOT="${HOME}/dev/system_config"
 
-
-source "${VA_SYSTEM_CONFIG_ROOT}/bashrc/ps1_colors.sh"
-source "${VA_SYSTEM_CONFIG_ROOT}/bashrc/docker_env.sh"
+#: PS1
 source "${VA_SYSTEM_CONFIG_ROOT}/bashrc/ps1_git.sh"
+source "${VA_SYSTEM_CONFIG_ROOT}/bashrc/ps1_colors.sh"
+
+#: Docker environment manager
+source "${VA_SYSTEM_CONFIG_ROOT}/bashrc/docker_env.sh"
