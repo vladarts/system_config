e62ba0d1f3eedab41b79cea8a872027827b1f359
vpiskunov <vpiskunov@iponweb.net>
2019-08-28 13:45:00 +0300

    Add updates

--- bashrc/macos_bash_profile.sh
+++ bashrc/macos_bash_profile.sh
@@ -30,3 +30,24 @@ alias postfix_logs="log stream --predicate  '(process == "smtpd") || (process ==
 
 #: Bash completion
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+
+#: Sublime
+if [ -d '/Applications/Sublime Text.app/Contents/SharedSupport/bin' ]; then 
+	export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"; 
+fi
+
+#: pyenv fix
+export LDFLAGS="${LDFLAGS} -L/usr/local/opt/zlib/lib"
+export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/zlib/include"
+export LDFLAGS="${LDFLAGS} -L/usr/local/opt/sqlite/lib"
+export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/sqlite/include"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/zlib/lib/pkgconfig"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/sqlite/lib/pkgconfig"
+
+#: GO
+export GOPATH="${HOME}/dev/go"
+
+#: IOW
+if [ -f "${HOME}/vpiskunov@iponweb.net/dev_tmp/bashrc/main.sh" ]; then 
+	. ${HOME}/vpiskunov@iponweb.net/dev_tmp/bashrc/main.sh; 
+fi
